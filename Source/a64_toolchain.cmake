5b3ca08efc89d5361cee8ecfadd475410d40a41e
ezio <ezio_zhang@outlook.com>
2017-03-27 00:40:44 +0800

    update

--- Source/a64_toolchain.cmake
+++ Source/a64_toolchain.cmake
@@ -2,8 +2,9 @@ SET(A64_PATH "/opt/linaro/gcc-linaro_aarch64_bare/bin/")    #your aarch64 compil
 SET(A64_LD "${A64_PATH}aarch64-elf-ld")
 SET(A64_CC "${A64_PATH}aarch64-elf-gcc")
 SET(A64_OD "${A64_PATH}aarch64-elf-objdump")
+SET(A64_OC "${A64_PATH}aarch64-elf-objcopy")
 
 SET(CMAKE_SYSTEM_NAME Generic)
 SET(CMAKE_SYSTEM_PROCESSOR ARM)
 SET(CMAKE_C_COMPILER ${A64_CC})
-SET(CMAKE_C_LINK_EXECUTABLE " ${A64_LD}  <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET> ")
+SET(CMAKE_C_LINK_EXECUTABLE "${A64_LD} <CMAKE_C_LINK_FLAGS> <LINK_FLAGS> <OBJECTS>  -o <TARGET> ")
